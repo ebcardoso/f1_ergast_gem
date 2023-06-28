e9a82f747a243c493cccb52e29971ee72e7d2555
Breno Cardoso <elisiobreno@gmail.com>
2023-06-28 12:16:07 -0300

    Drivers: Endpoints "list by year" and "list by round"

--- lib/f1_ergast/modules/drivers.rb
+++ lib/f1_ergast/modules/drivers.rb
@@ -6,6 +6,18 @@ module F1Ergast
       result
     end
 
+    def by_year(year)
+      url = "#{year}/#{DRIVERS}"
+      result = get_request(url)
+      result
+    end
+
+    def by_year_in_round(year, round)
+      url = "#{year}/#{round}/#{DRIVERS}"
+      result = get_request(url)
+      result
+    end
+
     def describe(id)
       url = "#{DRIVERS}/#{id}"
       result = get_request(url)
