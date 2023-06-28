abbb3b4f624d8b554f7e8d1372f30abff3244ac0
Breno Cardoso <elisiobreno@gmail.com>
2023-06-27 23:59:44 -0300

    Constructors EPS

--- lib/f1_ergast/modules/ergast_module.rb
+++ lib/f1_ergast/modules/ergast_module.rb
@@ -1,2 +1,3 @@
 require_relative 'ergast_services'
+require_relative 'constructors'
 require_relative 'results'
--- lib/f1_ergast/utils/urls.rb
+++ lib/f1_ergast/utils/urls.rb
@@ -1,6 +1,9 @@
 module F1Ergast
   ROOT_URL = 'http://ergast.com/api/f1/'
 
+  # Constructors
+  CONSTRUCTORS = 'constructors'
+
   # Results
   RACE_RESULT = 'results'
   MOST_RECENT = 'current/last/results'
